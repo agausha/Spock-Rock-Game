62528e6c264657b50c6e18947702dc51f6e2bc09
Isaac <uniqucode@gmail.com>
2022-09-08 23:02:09 +0100

    Added choices for lizard

--- js/script.js
+++ js/script.js
@@ -22,4 +22,5 @@ const choices = {
   rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
   paper: { name: 'Paper', defeats: ['rock', 'spock'] },
   scissors: { name: 'Scissors', defeats: ['paper', 'lizard'] },
+  lizard: { name: 'Lizard', defeats: ['paper', 'spock'] },
 }
