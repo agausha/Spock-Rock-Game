cd7fa60dc9459537d37273a6b9ecfd2e9eb0aec3
Isaac <uniqucode@gmail.com>
2022-09-08 22:35:12 +0100

    Refrencing playerRock

--- js/script.js
+++ js/script.js
@@ -3,3 +3,5 @@ const playerChoiceEl = document.getElementById('playerChoice');
 const computerScoreEl = document.getElementById('computerScore');
 const computerChoiceEl = document.getElementById('computerChoice');
 const resultText = document.getElementById('resultText');
+
+const playerRock = document.getElementById('playerRock');
