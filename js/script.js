60d9578a78fe07d9d561a6eaca623ef7d2897b20
Isaac <uniqucode@gmail.com>
2022-09-08 22:32:01 +0100

    Refrencing computerChoice

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 const playerScoreEl = document.getElementById('playerScore');
 const playerChoiceEl = document.getElementById('playerChoice');
 const computerScoreEl = document.getElementById('computerScore');
+const computerChoiceEl = document.getElementById('computerChoice');
